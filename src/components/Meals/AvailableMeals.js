9b0f2aa1b21cfd001605da550a847d703005635c
Desenvolvedor <desenvolvedor@MacBook-Pro-de-Desenvolvedor.local>
2021-05-26 22:13:08 -0300

    form input

---
+++ src/components/Meals/AvailableMeals.js
@@ -0,0 +1,47 @@
+import Card from '../UI/Card';
+import MealItem from './MealItem/MealItem';
+import styles from './AvailableMeals.module.css';
+
+const DUMMY_MEALS = [
+    {
+      id: 'm1',
+      name: 'Sushi',
+      description: 'Finest fish and veggies',
+      price: 22.99,
+    },
+    {
+      id: 'm2',
+      name: 'Schnitzel',
+      description: 'A german specialty!',
+      price: 16.5,
+    },
+    {
+      id: 'm3',
+      name: 'Barbecue Burger',
+      description: 'American, raw, meaty',
+      price: 12.99,
+    },
+    {
+      id: 'm4',
+      name: 'Green Bowl',
+      description: 'Healthy...and green...',
+      price: 18.99,
+    },
+  ];
+  
+const AvailableMeals = () => {
+
+    const mealsList = DUMMY_MEALS.map( m =>
+        <MealItem key={m.id} name={m.name} description={m.description} price={m.price}/>
+    );
+
+    return(
+            <section className="styles.meals">
+                <Card>
+                  <ul>{ mealsList }</ul>
+                </Card>
+            </section>
+    );
+}
+
+export default AvailableMeals;
